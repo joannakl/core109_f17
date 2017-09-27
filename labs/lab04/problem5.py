90891a3d839844d761b325831c4d2711bd523476
JoannaKl <joannakl@cs.nyu.edu>
2017-09-27 16:06:53 -0400

    for loop problems slides and lab4 exercises

---
+++ labs/lab04/problem5.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+"""
+problem5.py
+====
+
+Author:  
+
+
+
+
+Write a program that determines the user's salary at the end of each year
+from year 1 to 10. 
+Your program should prompt the user for the initial salary and for the percent
+increasy that is going to be applied at the end of each year (same
+for all of the years).
+
+Hint: For dollar amounts we want to limit the number of decimal places to just two
+(we are not going to be listing fractions of a cent). 
+To do so, you can use the format() function as follows:
+    assume that that variable salary is equal to 10.038791
+        format (salary, '.2f') 
+    returns a string  "10.03"
+Try to use this function in your pgoram to print the dollar amounts with two decimal
+places. 
+   
+==============
+Possible interaction: 
+    
+    
+Enter your initial salary: 50000
+
+Enter the percent increase at the end of each year [2, for 2%, for example]: 2
+
+year    salary
+1       $51000.00
+2       $52020.00
+3       $53060.40
+4       $54121.61
+5       $55204.04
+6       $56308.12
+7       $57434.28
+8       $58582.97
+9       $59754.63
+10      $60949.72
+
+==============
+
+__COMMENT YOUR SOURCE CODE__ by 
+
+* briefly describing parts of your program 
+* including your name at the top of your
+  file (above these instructions)
+"""
+    
+    
