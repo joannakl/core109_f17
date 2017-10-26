ee479e4f58b2ac73d2a8913104b549a6ed4c614a
JoannaKl <joannakl@cs.nyu.edu>
2017-10-25 21:25:50 -0400

    lab7

---
+++ labs/lab07/problem1.py
@@ -0,0 +1,54 @@
+"""
+problem1.py
+====
+
+Author:  
+
+
+Write a program that calculates the sum and average of elements stored in 
+a given list of numbers.
+
+
+The program below has a few definitions of specific lists called numbers
+but your program should work even if the code is edited to change the 
+content of that list to an arbitrary list of numbers. 
+
+Restriction: you cannot use the sum() function that was mentioned in class. You have to
+use a loop to calculate the sum of the values stored in the list. 
+
+
+
+==============
+Here are a few sample runs of your program:
+    
+list: [1, -3, 5, -17, 13, 72, -98, 615]
+sum:  588
+avg:  73.5
+
+--
+    
+list: [3, 5, 15, -8, 43, 78, -9]
+sum:  127
+avg:  18.142857142857142
+
+--
+    
+list: [-99, -96, -93, -90, -87, -84, -81, -78, -75, -72, -69, -66, -63, -60, -57, -54, -51, -48, -45, -42, -39, -36, -33, -30, -27, -24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99]
+sum:  0
+avg:  0.0
+
+==============
+
+__COMMENT YOUR SOURCE CODE__ by 
+
+* briefly describing parts of your program 
+* including your name at the top of your
+  file (above these instructions)
+"""
+
+
+numbers = [1, -3, 5, -17, 13, 72, -98, 615]
+
+#numbers = [3, 5, 15, -8, 43, 78, -9]
+
+#numbers = list( range(-99, 100, 3) ) 
---
+++ labs/lab07/problem4.py
@@ -0,0 +1,77 @@
+"""
+problem4.py
+====
+
+Author:  
+
+
+
+Write a program that asks the user to continually enter student grades. 
+To indicate the end of the list, the user will enter "-1" 
+for the grade. 
+
+Your program should validate that the grades are within a valid range of 0 to 100. 
+
+The program should store the grades in a list. 
+
+Calculate the following information based on the data:
+
+- number of students who took the exam
+
+- average grade on the exam 
+
+- standard deviation of the grades 
+  (see https://joannakl.github.io/core109_f17/slides/week6/grades.pdf if you do not
+  remember how to calculate the standard deviation) 
+  
+Note: taking a square room of a number is the same as raising that number 
+to exponent equal to 0.5. If you want to take a square root of x in Python, you can 
+calculate:
+    sqrt_x = x ** 0.5 
+  
+
+
+==============
+Here are a few sample runs of your program:
+    
+Enter the grades one at a time. Enter -1 to indicate the end.
+
+next grade: 90
+
+next grade: -7
+invalid score, not counted
+
+next grade: 70
+
+next grade: 85
+
+next grade: -1
+
+ ----------
+list of grades:  [90, 70, 85]
+number of grades:  3
+average score:     81.66666666666667
+standard dev:      8.498365855987975
+
+===
+
+Enter the grades one at a time. Enter -1 to indicate the end.
+
+next grade: -1
+
+ ----------
+list of grades:  []
+number of grades:  0
+
+==============
+
+__COMMENT YOUR SOURCE CODE__ by 
+
+* briefly describing parts of your program 
+* including your name at the top of your
+  file (above these instructions)
+"""
+
+
+    
+    
