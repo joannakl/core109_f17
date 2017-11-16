cbd7a3129be91d1ac730b9eb486f82d03658ecb9
JoannaKl <joannakl@cs.nyu.edu>
2017-11-15 22:34:44 -0500

    lab 9

--- labs/lab09/problem4_solution.py
+++ labs/lab09/problem4_solution.py
@@ -40,8 +40,19 @@ import urllib.request
 
 # read the text 
 
+# 
+url = "http://www.gutenberg.org/cache/epub/5347/pg5347.txt" 
+
+# Ulysses by James Joyce 
+# url = "https://www.gutenberg.org/ebooks/4300" 
+
+# Pride and Prejudice by Jane Austen 
+# url = "https://www.gutenberg.org/files/1342/1342-0.txt" 
+
 # War and Peace
-url = "http://www.gutenberg.org/files/2600/2600-0.txt"
+# url = "http://www.gutenberg.org/files/2600/2600-0.txt"
+
+
 response = urllib.request.urlopen(url)
 # read data from URL as a String, making sure
 # that the String is formatted as a series of ASCII characters
@@ -62,7 +73,7 @@ words_total = len(book_words)
 # count hard words 
 hard_words_total = 0
 for word in book_words:
-    if not easy_words.find(word) :
+    if word not in easy_words:
         hard_words_total += 1 
         
 # count sentences
@@ -70,4 +81,24 @@ data = data.replace("! ", ". ")
 data = data.replace("? ", ". ")
 data = data.replace("? ", ". ")
 book_sentences = data.split(".") 
+sentence_total = len(book_sentences) - 1
 
+dale_chall_score = 0.1579 * (100 * hard_words_total/words_total) + \
+        0.0496 * (words_total / sentence_total) 
+        
+print ("Dale-Chall score:", dale_chall_score) 
+
+if dale_chall_score <= 4.9 :
+    print("easily understood by an average 4th-grade student or lower")
+elif dale_chall_score <= 5.9:
+    print("easily understood by an average 5th or 6th-grade student")
+elif dale_chall_score <= 6.9:
+    print("easily understood by an average 7th or 8th-grade student") 
+elif dale_chall_score <= 7.9:
+    print("easily understood by an average 9th or 10th-grade student") 
+elif dale_chall_score <= 8.9:
+    print("easily understood by an average 11th or 12th-grade student") 
+elif dale_chall_score <= 9.9:
+    print("easily understood by an average 13th to 15th-grade (college) student") 
+else: 
+    print ("hard to understand!") 
